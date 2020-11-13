f1bf5b7342d756467c36627cbb4c112507ab61ef
augustoliks <carlos.santos110@fatec.sp.gov.br>
2020-11-12 21:56:01 -0300

    resolvido exercicios

--- sql/2603.sql
+++ sql/2603.sql
@@ -1,7 +1,7 @@
 -- --- URI Online Judge SQL
 -- --- Copyright URI Online Judge
 -- --- www.urionlinejudge.com.br
--- --- Problem 2602
+-- --- Problem 2603
 
 -- CREATE TABLE customers (
 --   id NUMERIC PRIMARY KEY,
@@ -13,13 +13,12 @@
 -- );
 
 -- INSERT INTO customers (id, name, street, city, state, credit_limit)
--- VALUES 
+-- VALUES
 --   (1,	'Pedro Augusto da Rocha',	'Rua Pedro Carlos Hoffman',	'Porto Alegre',	'RS',	700.00),
---   (2,	'Antonio Carlos Mamel',	'Av. Pinheiros', 'Belo Horizonte',	'MG',	3500.50),
---   (3,	'Luiza Augusta Mhor',	'Rua Salto Grande',	'Niteroi',	'RJ',	4000.00),	
+--   (2,	'Antonio Carlos Mamel',	'Av. Pinheiros',	'Belo Horizonte',	'MG',	3500.50),	
+--   (3,	'Luiza Augusta Mhor,',	'Rua Salto Grande',	'Niteroi',	'RJ',	4000.00),
 --   (4,	'Jane Ester',	'Av 7 de setembro',	'Erechim',	'RS',	800.00),
---   (5, 'Marcos Antônio dos Santos',	'Av Farrapos',	'Porto Alegre',	'RS',	4250.25);
-
+--   (5,	'Marcos Antônio dos Santos',	'Av Farrapos',	'Porto Alegre',	'RS',	4250.25);
   
 --   /*  Execute this query to drop the tables */
 --   -- DROP TABLE customers; --
